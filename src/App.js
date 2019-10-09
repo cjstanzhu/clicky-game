6be4966401296b2500b858ae66ccd038cbb393d9
cjstanzhu <cjstanzhu@gmail.com>
2019-10-09 16:11:04 -0400

    setup files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Score from "./components/Score";
+import ImageCard from "./components/ImageCard";
+
+class App extends Component {
+  render() {
+    return <Game />;
+  };
+};
+
+export default App;
+
