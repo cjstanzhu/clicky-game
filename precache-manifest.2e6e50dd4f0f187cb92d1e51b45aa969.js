d94515d4b82b93f3179e470d6c84c6e400d92b2f
cjstanzhu <cjstanzhu@gmail.com>
2019-10-10 13:01:26 -0400

    Updates

---
+++ precache-manifest.2e6e50dd4f0f187cb92d1e51b45aa969.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3503e24ebea1a60ba30e6c33fe2f449b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "49291cc99316bcbb9a5c",
+    "url": "/clicky-game/static/js/2.746b3695.chunk.js"
+  },
+  {
+    "revision": "c4b696ec40fb51da6e58",
+    "url": "/clicky-game/static/js/main.ae1075ac.chunk.js"
+  },
+  {
+    "revision": "61fe689653f34435d57f",
+    "url": "/clicky-game/static/js/runtime-main.8c94c7f2.js"
+  }
+]);
